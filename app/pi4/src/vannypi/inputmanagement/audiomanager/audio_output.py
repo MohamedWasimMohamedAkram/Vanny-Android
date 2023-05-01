a4ec62fdae93cee7e4deefd106c7be8b481312a1
Abdumbo99 <78922825+Abdumbo99@users.noreply.github.com>
2023-05-01 17:07:36 +0300

    No Pi code here

--- app/pi4/src/vannypi/inputmanagement/audiomanager/audio_output.py
+++
@@ -1,6 +0,0 @@
-from vannypi.inputmanagement.audiomanager.audio import Audio
-
-
-class AudioOutput:
-    def __int__(self):
-        self._output: Audio = Audio()
--- app/pi4/src/vannypi/inputmanagement/videomanager/video.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-class Video:
-    def __int__(self):
-        self._data: List[float] = []  # type subject to change in the future
-        self._id: str = ""
