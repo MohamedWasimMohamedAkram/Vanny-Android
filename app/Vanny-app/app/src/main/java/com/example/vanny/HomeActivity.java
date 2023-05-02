6bf7f02ee485ed35c191abe76bb98b5856cd33b9
Mohamed Wasim Mohamed Akram <63498839+MohamedWasimMohamedAkram@users.noreply.github.com>
2023-05-03 00:22:33 +0300

    Vanny Android
    Added MQTT communication

--- app/Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
+++ app/Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
@@ -2,25 +2,103 @@ package com.example.vanny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private MqttAndroidClient mqttAndroidClient;
+    private Handler handler;
+    private final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        imageView = findViewById(R.id.streamContainer);
+        handler = new Handler();
+
+        String brokerUrl = "tcp://<broker_ip_address>:1883";
+        String clientId = "android_client";
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setCleanSession(true);
+        connectOptions.setUserName("<username>");
+        connectOptions.setPassword("<password>".toCharArray());
+        MemoryPersistence persistence = new MemoryPersistence();
+        mqttAndroidClient = new MqttAndroidClient(this, brokerUrl, clientId, persistence);
+
         Button pairButton = findViewById(R.id.pairButton);
         pairButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RemoteConnection remoteConnection = new RemoteConnection();
-                RemoteConnection.connectToPi();
+                try {
+                    IMqttToken token = mqttAndroidClient.connect(connectOptions);
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            Log.i(TAG, "Connected to MQTT broker");
+                            try {
+                                mqttAndroidClient.subscribe("my/topic", 0, new IMqttMessageListener() {
+                                    @Override
+                                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                                        handler.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                byte[] payload = message.getPayload();
+                                                Bitmap bitmap = BitmapFactory.decodeByteArray(payload, 0, payload.length);
+                                                imageView.setImageBitmap(bitmap);
+                                            }
+                                        });
+                                    }
+                                });
+                            } catch (MqttException ex) {
+                                Log.e(TAG, "Failed to subscribe to MQTT topic", ex);
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            Log.e(TAG, "Failed to connect to MQTT broker", exception);
+                        }
+                    });
+                } catch (MqttException ex) {
+                    Log.e(TAG, "Failed to connect to MQTT broker", ex);
+                }
             }
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            mqttAndroidClient.disconnect();
+        } catch (MqttException ex) {
+            Log.e(TAG, "Failed to disconnect from MQTT broker", ex);
+        }
+    }
 }
--- app/Vanny-app/app/src/main/java/com/example/vanny/MQTTConnection.java
+++ app/Vanny-app/app/src/main/java/com/example/vanny/MQTTConnection.java
@@ -1,5 +1,6 @@
 package com.example.vanny;
 
+import androidx.appcompat.app.AppCompatActivity;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
@@ -18,23 +19,18 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import java.util.Arrays;
 
-public class RemoteConnection {
-    public static void connectToPi() {
+import android.os.Bundle;
+
+public class MQTTConnection extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mqttconnection);
+
     }
+
+
 }
-
-
-
-
-
-//        try {
-//            JSch jsch = new JSch();
-//            Session session = jsch.getSession("pi", "<YOUR_RASPBERRY_PI_IP_ADDRESS>", 22);
-//            session.setPassword("<YOUR_RASPBERRY_PI_PASSWORD>");
-//            session.setConfig("StrictHostKeyChecking", "no");
-//            session.connect();
-//            // Do something with the session here
-//            session.disconnect();
-//        } catch (JSchException e) {
-//            e.printStackTrace();
-//        }
