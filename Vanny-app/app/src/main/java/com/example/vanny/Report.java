fc669ad53aa550c659cee1cb0e571741e91d06e9
jhchowdhury <jhschowdhury@gmail.com>
2023-05-10 22:25:56 +0300

    Added Reports. Not complete.

--- Vanny-app/app/src/main/java/com/example/vanny/Report.java
+++ Vanny-app/app/src/main/java/com/example/vanny/Report.java
@@ -1,14 +1,15 @@
 package com.example.vanny;
 public class Report {
 
-    String type, Title, Message, filePath;
+    String type, Title, Message, filePath, timeStamp;
     int imageId;
 
-    public Report(String type, String Title, String Message, String filePath, int imageId) {
+    public Report(String type, String Title, String Message, String filePath, String timeStamp, int imageId) {
         this.type = type;
         this.Title = Title;
         this.Message = Message;
         this.filePath = filePath;
+        this.timeStamp = timeStamp;
         this.imageId = imageId;
     }
 }
