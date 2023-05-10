1b590742a2b0e34b019f5e3130c85f3bd0222aeb
Mohamed Wasim Mohamed Akram <63498839+MohamedWasimMohamedAkram@users.noreply.github.com>
2023-05-10 19:11:06 +0300

    Vanny Streaming
    Streaming connection successful

--- Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
+++ Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
@@ -7,59 +7,54 @@ import org.zeromq.ZContext;
 import java.io.ByteArrayInputStream;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ImageView imageView;
-    private Handler handler;
-    private ZMQ.Socket socket;
-    private Thread subscriberThread;
-    private volatile boolean running;
+    private VideoView videoView;
+    private String streamUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //imageView = findViewById(R.id.streamContainer);
-        handler = new Handler(Looper.getMainLooper());
-        running = true;
-        try (ZContext context = new ZContext()) {
-            // Socket to talk to clients
-            ZMQ.Socket socket = context.createSocket(SocketType.REP);
-            socket.bind("tcp://*:5555");
+        MediaController mediaController = new MediaController(this);
+        Button pairButton = findViewById(R.id.pairButton);
 
-            while (!Thread.currentThread().isInterrupted()) {
-                // Block until a message is received
-                byte[] reply = socket.recv(0);
+        pairButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get a reference to the VideoView
+                videoView = findViewById(R.id.streamContainer);
 
-                // Print the message
-                System.out.println(
-                        "Received: [" + new String(reply, ZMQ.CHARSET) + "]"
-                );
+                // Create a MediaController to control the playback
+                mediaController.setAnchorView(videoView);
 
-                // Send a response
-                String response = "Hello, world!";
-                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+                // Set the MediaController to the VideoView
+                videoView.setMediaController(mediaController);
+
+                // Set the video URI to the MJPEG-Streamer URL
+                Uri videoUri = Uri.parse(streamUrl);
+                videoView.setVideoURI(videoUri);
+
+                // Start the video playback
+                videoView.start();
             }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        running = false;
-        try {
-            subscriberThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        });
     }
 }
