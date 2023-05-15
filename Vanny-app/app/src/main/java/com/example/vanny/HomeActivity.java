fa03724e5a7e40636f3a4d8c25799dca5f6012bb
Mohamed Wasim Mohamed Akram <63498839+MohamedWasimMohamedAkram@users.noreply.github.com>
2023-05-15 19:02:45 +0300

    Vanny Streaming
    OpenCV and Flask streaming

--- Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
+++ Vanny-app/app/src/main/java/com/example/vanny/HomeActivity.java
@@ -1,38 +1,91 @@
 package com.example.vanny;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.Toast;
-import android.widget.VideoView;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 import java.io.IOException;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String STREAM_URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-    private VideoView videoView;
+    private ImageView imageView;
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        videoView = findViewById(R.id.stream_container);
-        videoView.setVideoURI(Uri.parse(STREAM_URL));
-        videoView.start();
+        imageView = findViewById(R.id.stream_container);
+        running = true;
+
+        // Start a separate thread for receiving the video stream
+        Thread receiveThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket("10.0.2.2",5000);
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    Log.d("Socket", "Connected to server");
+
+                    while (running) {
+                        // Read the length of the incoming image
+                        int length = dataInputStream.readInt();
+                        if (length > 0) {
+                            byte[] imageBytes = new byte[length];
+                            dataInputStream.readFully(imageBytes, 0, imageBytes.length);
+
+                            // Convert the image bytes to a Bitmap
+                            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+                            final Bitmap bitmap = BitmapFactory.decodeStream(byteArrayInputStream);
+
+                            // Display the bitmap on the ImageView in the main UI thread
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imageView.setImageBitmap(bitmap);
+                                }
+                            });
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("Socket", "Connection Failed");
+                }
+            }
+        });
+        receiveThread.start();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        running = false;
+
+        try {
+            if (dataInputStream != null)
+                dataInputStream.close();
+
+            if (socket != null)
+                socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
